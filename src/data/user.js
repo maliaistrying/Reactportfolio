724932b9c894ec5f746794245080b4a6c735f974
malia <maliawaris1456@gmail.com>
2025-01-14 18:59:27 +0300

    Initial commit

---
+++ src/data/user.js
@@ -0,0 +1,84 @@
+const INFO = {
+	main: {
+		title: "Reactfolio by Maleehaa slay",
+		name: "Maleeha",
+		email: "Hi, I’m Maleeha Farooqui, a dedicated Web Developer and IT Specialist with a degree in Information Technology. I have hands-on experience in creating secure, user-friendly applications, including building an authentication system using Express.js and Mongoose and designing a CRM tool to streamline client management. My expertise extends to crafting responsive, dynamic front-end interfaces with HTML, CSS, and JavaScript, as well as exploring cloud computing fundamentals. I’m currently seeking opportunities in Web Development, Frontend Development, or IT Support—open to onsite roles in Bahrain or remote opportunities in Malaysia and GCC. Let’s connect and create something amazing!",
+		logo: "../maleeha.jpeg",
+	},
+
+	socials: {
+		twitter: "https://twitter.com/",
+		github: "https://github.com/",
+		linkedin: "https://linkedin.com/",
+		instagram: "https://instagram.com/",
+		stackoverflow: "https://stackoverflow.com/",
+		facebook: "https://facebook.com/",
+	},
+
+	homepage: {
+		title: "Full-stack web and mobile app developer, and Coffee lover.",
+		description:
+			"I am a Fullstack devloper with expertise in React.js. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards. I am always looking for new challenges and opportunities to grow as a developer.",
+	},
+
+	about: {
+		title: "I’m Maleeha. I live in Bahrain, where I design the future.",
+		description:
+			"I've worked on a variety of projects over the years and I'm proud of the progress I've made. Many of these projects are open-source and available for others to explore and contribute to. If you're interested in any of the projects I've worked on, please feel free to check out the code and suggest any improvements or enhancements you might have in mind. Collaborating with others is a great way to learn and grow, and I'm always open to new ideas and feedback.",
+	},
+
+	articles: {
+		title: "I'm passionate about pushing the boundaries of what's possible and inspiring the next generation of innovators.",
+		description:
+			"Chronological collection of my long-form thoughts on programming, leadership, product design, and more.",
+	},
+
+	projects: [
+		{
+			title: "Project 1",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 2",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/python/python.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 3",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/html/html.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 4",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+
+		{
+			title: "Project 5",
+			description:
+				"Lorem ipsum dolor sit amet. Et incidunt voluptatem ex tempore repellendus qui dicta nemo sit deleniti minima.",
+			logo: "https://cdn.jsdelivr.net/npm/programming-languages-logos/src/javascript/javascript.png",
+			linkText: "View Project",
+			link: "https://github.com",
+		},
+	],
+};
+
+export default INFO;
---
+++ src/pages/projects.jsx
@@ -0,0 +1,74 @@
+import React, { useEffect } from "react";
+import { Helmet } from "react-helmet";
+
+import NavBar from "../components/common/navBar";
+import Footer from "../components/common/footer";
+import Logo from "../components/common/logo";
+import AllProjects from "../components/projects/allProjects";
+
+import INFO from "../data/user";
+import SEO from "../data/seo";
+
+import "./styles/projects.css";
+
+const Projects = () => {
+	useEffect(() => {
+		window.scrollTo(0, 0);
+	}, []);
+
+	const currentSEO = SEO.find((item) => item.page === "projects");
+
+	return (
+		<React.Fragment>
+			<Helmet>
+				<title>{`Projects | ${INFO.main.title}`}</title>
+				<meta name="description" content={currentSEO.description} />
+				<meta
+					name="keywords"
+					content={currentSEO.keywords.join(", ")}
+				/>
+			</Helmet>
+
+			<div className="page-content">
+				<NavBar active="projects" />
+				<div className="content-wrapper">
+					<div className="projects-logo-container">
+						<div className="projects-logo">
+							<Logo width={46} />
+						</div>
+					</div>
+					<div className="projects-container">
+						<div className="title projects-title">
+							Things I’ve made trying to put my dent in the
+							universe.
+						</div>
+
+						<div className="subtitle projects-subtitle">
+							
+
+👩‍💻 About Me
+
+Hi, I’m Maleeha Farooqui, a passionate Web Developer and IT Specialist with a degree in Information Technology. I specialize in building efficient, user-friendly applications and have experience with full-stack development, UX design, and cloud computing.
+
+💡 Projects I’ve Worked On:
+
+Auth: A secure authentication system with encryption using Express.js and Mongoose.
+CRM System: Designed a tool to enhance client management and business processes.
+Front-End Development: Built responsive, dynamic UIs with HTML, CSS, and JavaScript.
+🌟 I’m currently seeking roles in Web Development, Frontend, or IT Support (onsite in Bahrain or remote in Malaysia/GCC). Let’s connect!
+						</div>
+
+						<div className="projects-list">
+							<AllProjects />
+						</div>
+					</div>
+					<div className="page-footer">
+						<Footer />
+					</div>
+				</div>
+			</div>
+		</React.Fragment>
+	);
+};
+
+export default Projects;
